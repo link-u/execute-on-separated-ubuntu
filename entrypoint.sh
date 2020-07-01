c4427d5360db86caac0063232d7fd394d766baff
psi <ryo.hirafuji@link-u.co.jp>
2020-07-01 14:36:56 +0900

    Grouping message.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -eu
 
+echo "::group::Build container"
 docker build -f "/Dockerfile-${INPUT_ON}" -t builder .
+echo "::endgroup::"
 docker run --rm \
   --volume="$RUNNER_WORKSPACE/$(basename $GITHUB_REPOSITORY)":"${GITHUB_WORKSPACE}" \
   --workdir="${GITHUB_WORKSPACE}" \
